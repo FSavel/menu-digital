d2460d45df8d8bbb73b305df6b76b273e280c98a
FSavel <sanatorioespiritual4@gmail.com>
2026-06-30 21:18:29 +0200

    Add files via upload

---
+++ languages.py
@@ -0,0 +1,49 @@
+LANGUAGES = {
+
+    "pt": {
+
+        "welcome": "Bem-vindo ao Restaurante",
+        "choose_language": "Escolha o idioma",
+
+        "menu": "Menu",
+        "reservations": "Reservas",
+        "about": "Sobre",
+        "panel": "Painel",
+
+        "add": "Adicionar",
+        "cart": "Carrinho",
+
+        "reservation": "Fazer Reserva",
+        "send": "Enviar",
+
+        "success": "Pedido enviado com sucesso!",
+        "thank_you": "Obrigado pela preferência!",
+
+        "back": "Voltar",
+
+    },
+
+    "en": {
+
+        "welcome": "Welcome to Dourados Restaurant",
+        "choose_language": "Choose your language",
+
+        "menu": "Menu",
+        "reservations": "Reservations",
+        "about": "About",
+        "panel": "Dashboard",
+
+        "add": "Add",
+        "cart": "Cart",
+
+        "reservation": "Book a Table",
+        "send": "Submit",
+
+        "success": "Order sent successfully!",
+        "thank_you": "Thank you for choosing us!",
+
+        "back": "Back",
+
+    }
+
+}
